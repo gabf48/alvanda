59c8ec141a7616e6b76a7c0d970e19519b57502e
Gabriel Filip <gabf48@gmail.com>
2023-05-19 14:40:31 +0300

    checked link for terms and conditions
    checked link for privacy and policy
    
    checked link for recover password

--- pages/login_page.py
+++ pages/login_page.py
@@ -13,3 +13,17 @@ class LoginPage:
         type_email = self.driver.find_element(By.CSS_SELECTOR,EMAIL_INPUT).send_keys(email)
         type_password = self.driver.find_element(By.CSS_SELECTOR,PASSWORD_INPUT).send_keys(password)
         press_sign_in_button = self.driver.find_element(By.CSS_SELECTOR, BUTTON_SIGN_IN).click()
+
+    def check_links_for_terms_and_privacy(self):
+        x = self.driver.find_element(By.CSS_SELECTOR,'div.Login_firstContainerContent__wbbdK > form > p > a:nth-child(1)').get_attribute('href')
+        print(x)
+        y = self.driver.find_element(By.CSS_SELECTOR,
+                                     'div.Login_firstContainerContent__wbbdK > form > p > a:nth-child(2)').get_attribute(
+            'href')
+        print(y)
+
+        assert x == 'https://alvanda.com/terms-conditions'
+        assert y == 'https://alvanda.com/privacy-policy'
+
+    def press_recover_password_button(self):
+        self.driver.find_element(By.CSS_SELECTOR,'div.Login_firstContainerContent__wbbdK > div > button > div > div > p').click()
--- tests/workflow.py
+++ tests/workflow.py
@@ -9,6 +9,18 @@ def test_workflow():
     driver = webdriver.Chrome()
     login_page = LoginPage(driver)
 
+    #access Alvanda
+    driver.get('https://front-end-dev.alvanda.com/')
+    driver.maximize_window()
 
+    #assert links for Terms and Conditions and Privacy and Policy
+    login_page.check_links_for_terms_and_privacy()
+
+    #assert link for Recover password page
+    login_page.press_recover_password_button()
+    current_url = driver.current_url
+    assert current_url == 'https://front-end-dev.alvanda.com/reset-password'
+
+    #login
     login_page.login('gabriel.filip@wesrom.com','Parola1993!')
-    time.sleep(10)
+    time.sleep(5)
