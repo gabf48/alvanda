d04cbb3015925aa4b0eb374fb7ba19a6760d4343
Gabriel Filip <37806829+gabf48@users.noreply.github.com>
2024-02-05 23:42:35 +0200

    ready to run

--- pages/login_page.py
+++ pages/login_page.py
@@ -6,15 +6,18 @@ EMAIL_INPUT = '[name="email"]'
 PASSWORD_INPUT = '[name="password"]'
 BUTTON_SIGN_IN = '[type="submit"]'
 BUTTON_CONTINUE = '[role="dialog"] div:nth-child(2) [comp-state="enabled"]'
+
+
 class LoginPage:
 
     def __init__(self, driver):
         self.driver = driver
-    def login(self,email,password):
+
+    def login(self, email, password):
         self.driver.maximize_window()
         self.driver.get('https://front-end-dev.alvanda.com/')
-        type_email = self.driver.find_element(By.CSS_SELECTOR,EMAIL_INPUT).send_keys(email)
-        type_password = self.driver.find_element(By.CSS_SELECTOR,PASSWORD_INPUT).send_keys(password)
+        type_email = self.driver.find_element(By.CSS_SELECTOR, EMAIL_INPUT).send_keys(email)
+        type_password = self.driver.find_element(By.CSS_SELECTOR, PASSWORD_INPUT).send_keys(password)
         press_sign_in_button = self.driver.find_element(By.CSS_SELECTOR, BUTTON_SIGN_IN).click()
         time.sleep(3)
         try:
@@ -23,7 +26,9 @@ class LoginPage:
             print(f"Upgrade modal don't displayed: {e}")
 
     def check_links_for_terms_and_privacy(self):
-        x = self.driver.find_element(By.CSS_SELECTOR,'div.Login_firstContainerContent__wbbdK > form > p > a:nth-child(1)').get_attribute('href')
+        x = self.driver.find_element(By.CSS_SELECTOR,
+                                     'div.Login_firstContainerContent__wbbdK > form > p > a:nth-child(1)').get_attribute(
+            'href')
         print(x)
         y = self.driver.find_element(By.CSS_SELECTOR,
                                      'div.Login_firstContainerContent__wbbdK > form > p > a:nth-child(2)').get_attribute(
@@ -34,4 +39,5 @@ class LoginPage:
         assert y == 'https://alvanda.com/privacy-policy'
 
     def press_recover_password_button(self):
-        self.driver.find_element(By.CSS_SELECTOR,'div.Login_firstContainerContent__wbbdK > div > button > div > div > p').click()
+        self.driver.find_element(By.CSS_SELECTOR,
+                                 'div.Login_firstContainerContent__wbbdK > div > button > div > div > p').click()
--- tests/procedures/create_more_procedures.py
+++ tests/procedures/create_more_procedures.py
@@ -1,28 +1,22 @@
 import time
 from selenium import webdriver
-from pages.builder_page import BuilderPage
-from pages.component_page import ComponentPage
 from pages.login_page import LoginPage
 from pages.nav_bar import NavBar
 from pages.procedure_page import ProcedurePage
 
+driver = webdriver.Chrome()
+login_page = LoginPage(driver)
+nav_bar = NavBar(driver)
+procedure_page = ProcedurePage(driver)
 
-def test_create_procedures():
-    driver = webdriver.Chrome(
-        executable_path='C:/projects python/alvanda/browsers/chromedriver.exe')
-    login_page = LoginPage(driver)
-    nav_bar = NavBar(driver)
-    builder_page = BuilderPage(driver)
-    component_page = ComponentPage(driver)
-    procedure_page = ProcedurePage(driver)
+# login
+login_page.login('gabriel.filip+122fsadkfj3ioas23@wesrom.com','Parola1993!')
 
-    # login
-    login_page.login('gabriel.filip+122fsadkfj3ioas23@wesrom.com','Parola1993!')
+# go procedures page
+nav_bar.pressBuilder()
 
-    # go procedures page
-    nav_bar.pressBuilder()
+# create_procedures
+time.sleep(5)
+for _ in range(40):
+    procedure_page.create_procedure()
 
-    # create_procedures
-    time.sleep(5)
-    for _ in range(40):
-        procedure_page.create_procedure()
