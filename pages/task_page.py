865a3ec3097723ea763638f3bffebcd32a50defd
Gabriel Filip <37806829+gabf48@users.noreply.github.com>
2024-05-27 21:02:09 +0300

    update - test_create_task

--- pages/task_page.py
+++ pages/task_page.py
@@ -2,10 +2,12 @@ from selenium.webdriver.common.by import By
 
 from utils import generate_random
 from utils.base_page import BasePage
+from utils.generate_random import GenerateRandom
 
 NEW_TASK_BUTTON = 'div:nth-child(3) > button:nth-child(2) > div'
 TASK_NAME_INPUT = 'div:nth-child(1) > div.StyledInput_inputHostContainer__sHK8F > input'
-ASSIGNEE_DROPDOWN = '#downshift-24-toggle-button > div:nth-child(2) > svg'
+ASSIGNEE_DROPDOWN = 'div.TaskForm_assigneeCreatorContainer__0c5V7 > div > div button'
+CREATE_AND_START_BUTTON = 'body > reach-portal > div:nth-child(2) button:nth-child(2) > div > div.Button_titleContainer__7vVZt'
 class TaskPage:
 
     def __init__(self, driver):
@@ -14,8 +16,22 @@ class TaskPage:
     def press_create_new_task(self):
         self.driver.find_element(By.CSS_SELECTOR, NEW_TASK_BUTTON).click()
 
+    def select_specific_task_responsible(self):
+        base_page = BasePage(self.driver)
+        base_page.select_specific_element_from_list(ASSIGNEE_DROPDOWN, 'div > ul > li > span > div', "Gabriel Admin")
+
     def complete_task_modal(self):
         base_page = BasePage(self.driver)
+        generate_random = GenerateRandom(self.driver)
         random_string = generate_random.generate_random_string()
         self.driver.find_element(By.CSS_SELECTOR, TASK_NAME_INPUT).send_keys(random_string)
-        base_page.select_random_element_from_specific_list(ASSIGNEE_DROPDOWN,'#downshift-24-item-0 > span > div')
+        self.select_specific_task_responsible()
+        self.select_random_procedure()
+        self.press_create_and_start_button()
+
+    def select_random_procedure(self):
+        base_page = BasePage(self.driver)
+        base_page.select_random_element_from_specific_list('div.SelectComponents_toggleBtnChildren__29RMx.TaskForm_toggleBtnChildren__ccjp\+ > p','div > ul > li > span > div')
+
+    def press_create_and_start_button(self):
+        self.driver.find_element(By.CSS_SELECTOR, CREATE_AND_START_BUTTON).click()
--- utils/base_page.py
+++ utils/base_page.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium.webdriver.common.by import By
 import random
 
@@ -27,3 +29,24 @@ class BasePage:
 
         # Perform actions with the selected element
         random_element.click()
+
+    def select_specific_element_from_list(self, open_dropdown, list_selector, item_text):
+        # Open the dropdown
+        self.driver.find_element(By.CSS_SELECTOR, open_dropdown).click()
+
+        # Find a list of web elements
+        element_list = self.driver.find_elements(By.CSS_SELECTOR, list_selector)
+
+        # Filter the list to find the element that matches the item_text
+        matching_element = None
+        for element in element_list:
+            if element.text == item_text:
+                matching_element = element
+                break
+
+        # Check if the matching element is found
+        if matching_element:
+            # Perform actions with the selected element
+            matching_element.click()
+        else:
+            raise ValueError(f"No element with text '{item_text}' found in the list.")
