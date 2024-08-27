fc216a9cc9c9c1245bae606e1303bf2762bf9893
Gabriel <gabf48@gmail.com>
2024-08-27 05:48:39 -0700

    update python-selenium-tests.yml and requirements.txt

--- tests/in_progress/create_task.py
+++ tests/in_progress/create_task.py
@@ -1,8 +1,6 @@
 import time
 import pytest
 from selenium import webdriver
-from pages.builder_page import BuilderPage
-from pages.component_page import ComponentPage
 from pages.login_page import LoginPage
 from pages.nav_bar import NavBar
 from pages.task_page import TaskPage
@@ -23,8 +21,6 @@ def browser():
 def test_create_task(browser):
     login_page = LoginPage(browser)
     nav_bar = NavBar(browser)
-    builder_page = BuilderPage(browser)
-    component_page = ComponentPage(browser)
     task_page = TaskPage(browser)
     generate_random = GenerateRandom(browser)
 
@@ -35,12 +31,12 @@ def test_create_task(browser):
     time.sleep(2)
     task_page.press_create_new_task()
     time.sleep(2)
-    task_page.complete_task_modal_specific_responsible_and_procedure("Gabriel Admin","Correct procedure")
-    time.sleep(10)
-    task_page.type_email("test@test.com")
-    task_page.type_number("100")
-    task_page.type_text(generate_random.generate_random_string())
-    task_page.press_complete_task_button()
-    time.sleep(10)
-    task_page.check_modal_text()
-    time.sleep(5000000)
+    # task_page.complete_task_modal_specific_responsible_and_procedure("Gabriel Admin","Correct procedure")
+    # time.sleep(10)
+    # task_page.type_email("test@test.com")
+    # task_page.type_number("100")
+    # task_page.type_text(generate_random.generate_random_string())
+    # task_page.press_complete_task_button()
+    # time.sleep(10)
+    # task_page.check_modal_text()
+    # time.sleep(5000000)
